a820abb6c713c726861a3d4734ee45ec7f2df3db
GaneshPatil1806 <ganeshpatil180603@gmail.com>
2024-06-28 21:29:17 +0530

    product Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import products from "../utils/products.json"
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
     <div className="p-8">
       <Container>
-        <HomeBanner/>
+        <div>
+          <HomeBanner/>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.products.map((product: any) =>{
+              return <ProductCard key={product._id} data={product}/>
+          })}
+        </div>
       </Container>
     </div>
   )
---
+++ utils/formatPrice.tsx
@@ -0,0 +1,8 @@
+export const formatPrice = (amt: number) => {
+    return new Intl.NumberFormat(
+            'en-IN', {
+            style: 'currency',
+            currency: 'IND'
+        }
+    ).format(amt)
+}
